f9ac276ee3c0a4f11195d1f0d3fcfe2eee3d387d
vnjassi <varunjassi15@gmail.com>
2026-04-08 14:19:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Jab koi "Buy Now" button par click karega, toh ye message dikhega
+document.querySelector('.buy-btn').addEventListener('click', function() {
+    alert('Thank you for your interest in Green Zone Nature Ayurveda!');
+});
+
+console.log("Green Zone Website files are now connected!");
